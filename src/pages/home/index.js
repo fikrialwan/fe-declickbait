c0bcf343f413f304bfac74212b9080f9155616f7
unknownunnamed <fikriar19@student.untan.ac.id>
2021-09-07 22:36:43 +0700

    day 4 night

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,8 +1,22 @@
-import React from "react";
-import { Card, Form, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Card, Form, Button, Modal } from "react-bootstrap";
 import color from "../../utility/color";
+import cautionIMG from "../../assets/svg/error.svg";
+import safeIMG from "../../assets/svg/shield.svg";
 
 const Home = () => {
+  const [judul, setJudul] = useState("");
+  const [sumber, setSumber] = useState("");
+  const [sumberLainnya, setSumberLainnya] = useState("");
+  const [show, setShow] = useState(false);
+  const [isClickbait, setIsClickbait] = useState(true);
+
+  const HandleClasify = (event) => {
+    event.preventDefault();
+    setIsClickbait(!isClickbait);
+    setShow(true);
+  };
+
   return (
     <div
       className="body-center"
@@ -22,7 +36,7 @@ const Home = () => {
       >
         <Card.Title>Klasifikasi Berita Clickbait</Card.Title>
         <Card.Body>
-          <Form>
+          <Form onSubmit={(e) => HandleClasify(e)}>
             <Form.Group className="mb-3">
               <Form.Control
                 placeholder="Masukkan judul berita"
@@ -32,6 +46,7 @@ const Home = () => {
                   outline: 0,
                   boxShadow: "none",
                 }}
+                onChange={(e) => setJudul(e.target.value)}
               />
             </Form.Group>
             <Form.Group>
@@ -43,13 +58,31 @@ const Home = () => {
                   outline: 0,
                   boxShadow: "none",
                 }}
+                onChange={(e) => setSumber(e.target.value)}
               >
                 <option hidden>Masukkan sumber berita</option>
                 <option value="Tribunnews.com">Tribunnews.com</option>
                 <option value="Kompas.com">Kompas.com</option>
                 <option value="Suara.com">Suara.com</option>
+                <option value="lainnya">Lainnya</option>
               </Form.Select>
             </Form.Group>
+            {sumber === "lainnya" ? (
+              <Form.Group className="mb-3">
+                <Form.Control
+                  placeholder="Masukkan sumber berita"
+                  style={{
+                    borderColor: color.black,
+                    opacity: 0.5,
+                    outline: 0,
+                    boxShadow: "none",
+                  }}
+                  onChange={(e) => setSumberLainnya(e.target.value)}
+                />
+              </Form.Group>
+            ) : (
+              <div />
+            )}
             <Button
               type="submit"
               className="col-12 btn"
@@ -57,10 +90,49 @@ const Home = () => {
                 color: color.white,
                 backgroundColor: color.red,
                 border: "none",
+                outline: 0,
+                boxShadow: "none",
               }}
+              // onClick={()=>HandleClasify()}
             >
               Klasifikasi
             </Button>
+            <Modal show={show} onHide={() => setShow(false)}>
+              <Modal.Header closeButton>
+                <Modal.Title>Hasil Klasifikasi</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <div className="row">
+                  <div className="col-md-4" />
+                  <img
+                    src={isClickbait ? cautionIMG : safeIMG}
+                    alt="img-not-found"
+                    className="p-2 col-md-4"
+                    // width="100%"
+                    // height="auto"
+                  />
+                  <div className="col-md-4" />
+                </div>
+                <p style={{
+                  textAlign: "center",
+                  padding: 10
+                }}>
+                  Berita dengan judul berita "{judul}" dari "
+                  {sumber === "lainnya" ? sumberLainnya : sumber}" termasuk
+                  kategori{" "}
+                  <span
+                    className="btn"
+                    style={{
+                      cursor: "none",
+                      color: isClickbait ? color.gray : color.black,
+                      backgroundColor: isClickbait ? color.red : color.blue,
+                    }}
+                  >
+                    {isClickbait ? "CLickbait" : "Bukan Clickbait"}
+                  </span>
+                </p>
+              </Modal.Body>
+            </Modal>
           </Form>
         </Card.Body>
       </Card>
